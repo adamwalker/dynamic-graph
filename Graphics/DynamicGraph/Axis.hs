4af8d979627b756f139bcdf9a4e9609c29a1e2ba
Adam Walker <adamwalker10@gmail.com>
2014-07-22 14:25:00 -0400

    axis tidy

--- Graphics/DynamicGraph/Axis.hs
+++ Graphics/DynamicGraph/Axis.hs
@@ -9,69 +9,6 @@ import Data.Colour.Names
 import Graphics.Rendering.Cairo
 import Graphics.Rendering.Pango
 
-data Configuration = Configuration {
-    width           :: Double,
-    height          :: Double,
-    topMargin       :: Double,
-    bottomMargin    :: Double,
-    leftMargin      :: Double,
-    rightMargin     :: Double,
-
-    backgroundColor :: Colour Double,
-    axisColor       :: Colour Double,
-    axisWidth       :: Double,
-    xGridConfig     :: Maybe GridConfig,
-    yGridConfig     :: Maybe GridConfig
-}
-
-data GridConfig = GridConfig {
-    gridColor   :: Colour Double,
-    gridWidth   :: Double,
-    gridDash    :: [Double],
-    gridOffset  :: Double,
-    gridSpacing :: Double,
-    gridLabels  :: [String],
-    textColor   :: Colour Double
-}
-
-defaultXGridConfig = GridConfig {..}
-    where
-    gridColor   = gray
-    gridWidth   = 0.5
-    gridDash    = []
-    gridOffset  = 50
-    gridSpacing = 100
-    gridLabels  = ["l1", "l2", "l3", "l4", "l5", "l6"]
-    textColor   = white
-
-defaultYGridConfig = GridConfig {..}
-    where
-    gridColor   = gray
-    gridWidth   = 0.5
-    gridDash    = [3, 1.5]
-    gridOffset  = 0
-    gridSpacing = 50
-    gridLabels  = ["l1", "l2", "l3", "l4", "l5", "l6"]
-    textColor   = white
-
-defaultConfiguration = Configuration {..}
-    where
-    width           = 500
-    height          = 500
-    topMargin       = 50
-    bottomMargin    = 50
-    leftMargin      = 50
-    rightMargin     = 50
-
-    backgroundColor = black
-    axisColor       = lightgray
-    axisWidth       = 1
-    xGridConfig     = Just defaultXGridConfig
-    yGridConfig     = Just defaultYGridConfig
-
-whenMaybe :: (Functor m, Monad m) => Maybe a -> (a -> m b) -> m ()
-whenMaybe mb func = maybe (return ()) (void . func) mb
-
 -- Make a pango layout, fill it with text and return its extents
 makeLayout :: PangoContext -> String -> Render (PangoLayout, PangoRectangle)
 makeLayout ctx text = liftIO $ do
@@ -98,8 +35,9 @@ blankCanvas colour width height  = do
     rectangle 0 0 width height
     fill
 
-drawAxes :: Configuration -> Render ()
-drawAxes Configuration{..} = do
+drawAxes :: Double -> Double -> Double -> Double -> Double -> Double -> Colour Double -> Double -> Render ()
+drawAxes width height topMargin bottomMargin leftMargin rightMargin axisColor axisWidth = do
+    setDash [] 0
     setLineCap  LineCapRound
     setLineJoin LineJoinRound
     setLineWidth axisWidth
@@ -121,13 +59,15 @@ gridXCoords width offset leftMargin rightMargin spacing = takeWhile (< (width - 
 gridYCoords :: Double -> Double -> Double -> Double -> Double -> [Double]
 gridYCoords height offset topMargin bottomMargin spacing = takeWhile (> topMargin) $ iterate (flip (-) spacing) (height - bottomMargin - offset)
 
-xAxisLabels :: PangoContext -> [String] -> [Double] -> Double -> Render ()
-xAxisLabels ctx gridLabels gridXCoords yCoord = 
+xAxisLabels :: PangoContext -> Colour Double -> [String] -> [Double] -> Double -> Render ()
+xAxisLabels ctx textColor gridLabels gridXCoords yCoord = do
+    uncurryRGB setSourceRGB (toSRGB textColor)
     forM_ (zip gridLabels gridXCoords) $ \(label, xCoord) -> do
         layoutTopCentre ctx label xCoord yCoord
 
-yAxisLabels :: PangoContext -> [String] -> [Double] -> Double -> Render ()
-yAxisLabels ctx gridLabels gridYCoords xCoord = 
+yAxisLabels :: PangoContext -> Colour Double -> [String] -> [Double] -> Double -> Render ()
+yAxisLabels ctx textColor gridLabels gridYCoords xCoord = do
+    uncurryRGB setSourceRGB (toSRGB textColor)
     forM_ (zip gridLabels gridYCoords) $ \(label, yCoord) -> do
         layoutRightCentre ctx label xCoord yCoord
 
@@ -150,34 +90,4 @@ yAxisGrid gridColor gridWidth gridDash xStart xEnd gridYCoords = do
         moveTo xStart yCoord
         lineTo xEnd   yCoord
         stroke
-
-renderAxes c@Configuration{..} = do
-    blankCanvas backgroundColor width height
-    drawAxes c
-
-    ctx <- liftIO $ cairoCreateContext Nothing
-
-    --X grid
-    whenMaybe xGridConfig $ \GridConfig{..} -> do
-
-        let gridXCoords' = gridXCoords width gridOffset leftMargin rightMargin gridSpacing
-
-        --grid lines
-        xAxisGrid gridColor gridWidth gridDash (height - bottomMargin) topMargin gridXCoords'
-
-        --axis labels
-        uncurryRGB setSourceRGB (toSRGB textColor)
-        xAxisLabels ctx gridLabels gridXCoords' (height - bottomMargin)
-
-    --Y grid
-    whenMaybe yGridConfig $ \GridConfig{..} -> do
-
-        let gridYCoords' = gridYCoords height gridOffset topMargin bottomMargin gridSpacing 
-
-        --grid lines
-        yAxisGrid gridColor gridWidth gridDash (width - rightMargin) leftMargin gridYCoords'
-
-        --axis labels
-        uncurryRGB setSourceRGB (toSRGB textColor)
-        yAxisLabels ctx gridLabels gridYCoords' 50 
 
